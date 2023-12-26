27a237583876d57cf26d3a0053f70f8a071a0c8e
Daniela C. Barbosa <contato@danielacb.com>
2023-12-25 23:34:23 -0300

    advanced react hooks: 01 - useReducer: simple Counter

--- 03-advanced-react-hooks/src/exercise/01.js
+++ 03-advanced-react-hooks/src/exercise/01.js
@@ -3,16 +3,100 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+// Exercise
+// function countReducer(state, newState) {
+//   return newState
+// }
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, changeCount] = React.useReducer(countReducer, initialCount)
+
+//   const increment = () => changeCount(count + step)
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// Extra Credit 1 - Accept Step as Action
+// function countReducer(count, step) {
+//   return count + step
+// }
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, changeCount] = React.useReducer(countReducer, initialCount)
+
+//   const increment = () => changeCount(step)
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// Extra Credit 2 - setState with Object
+// function countReducer(state, action) {
+//   return {
+//     ...state,
+//     ...action,
+//   }
+// }
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [state, setState] = React.useReducer(countReducer, {
+//     count: initialCount,
+//   })
+
+//   const {count} = state
+
+//   const increment = () => setState({count: count + step})
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// Extra Credit 3 - Object or Function
+// countReducer as Object
+// const countReducer = (state, action) => ({
+//   ...state,
+//   ...action,
+// })
+
+// countReducer as Function
+// const countReducer = (state, action) => action(state)
+
+// countReducer as Function or Object
+// const countReducer = (state, action) => ({
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [state, setState] = React.useReducer(countReducer, {
+//     count: initialCount,
+//   })
+
+//   const {count} = state
+
+//   const increment = () =>
+//     setState(currentState => ({count: currentState.count + step}))
+//   return <button onClick={increment}>{count}</button>
+// }
+
+// Extra Credit 4 - Traditional Dispatch Object
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {
+        ...state,
+        count: state.count + action.step,
+      }
+    }
+
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`)
+  }
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+
+  const {count} = state
+
+  const increment = () => setState({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
